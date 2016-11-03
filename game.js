8e8a88e3066599d1fa07349bde0a02058de3c24a
rcodybhall <rcody@brandonhall.org>
2016-11-03 10:43:33 -0400

    Updated some HTML
    Updated Some HTML

--- game.js
+++ game.js
@@ -7,6 +7,6 @@ window.onload = function() {
       else {
         var fruit = ["oranges", "bananas", "lemons", "mangos", "kiwis"];
       }
-    document.getElementById("fruitOut").innerHTML = "<div><p>" + user + " likes " + fruit + "</p></div>";
+    document.getElementById("fruitOut").innerHTML = "<div><p>" + user + " likes " + fruit[1] + "</p></div>";
 
 }
