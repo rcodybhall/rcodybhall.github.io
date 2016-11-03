f6f18ee2f13902a8a2456a08609c6a2a4e8df24c
rcodybhall <rcody@brandonhall.org>
2016-11-03 10:39:58 -0400

    Updated Game JS
    Updated Game JS

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
+window.onload = function() {
+    var user = "Ryan";
+      if (user == "Ryan")
+      {
+        var fruit = ["Apples", "Oranges"];
+      }
+      else {
+        var fruit = ["oranges", "bananas", "lemons", "mangos", "kiwis"];
+      }
+    document.getElementById("fruitOut").innerHTML = "<div><p>" + user + " likes " + fruit + "</p></div>";
 
-var user = "Ryan";
-
-if (user == "Ryan")
-{
-  var fruit = ["Apples", "Oranges"];
 }
-else {
-  var fruit = ["oranges", "bananas", "lemons", "mangos", "kiwis"];
-}
-
-document.getElementById("fruitOut").innerHTML = "<div><p>" + user + " likes " + fruit + "</p></div>";
