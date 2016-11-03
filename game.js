52585d4aa861693ee893cec92e108b1480830974
rcodybhall <rcody@brandonhall.org>
2016-11-03 10:32:08 -0400

    Quotes around ID
    Quotes around ID

--- game.js
+++ game.js
@@ -9,4 +9,4 @@ else {
   var fruit = ["oranges", "bananas", "lemons", "mangos", "kiwis"];
 }
 
-document.getElementById(fruitOut).innerHTML = "<div><p>" + user + " likes " + fruit + "</p></div>";
+document.getElementById("fruitOut").innerHTML = "<div><p>" + user + " likes " + fruit + "</p></div>";
