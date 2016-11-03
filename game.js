de7db026c3fe9328672a93716bf1c8b0d2f763b8
rcodybhall <rcody@brandonhall.org>
2016-11-03 10:29:17 -0400

    Fixed Errors
    Fixed Errors

--- game.js
+++ game.js
@@ -3,10 +3,10 @@ var user = "Ryan";
 
 if (user == "Ryan")
 {
-  fruit = ["Apples", "Oranges"]
+  var fruit = ["Apples", "Oranges"]
 }
 else {
   var fruit = ["oranges", "bananas", "lemons", "mangos", "kiwis"];
 }
 
-document.getElementById(fruitOut).innerHTML = "<div><p>" + user + " likes " + fruit "</p></div>";
+document.getElementById(fruitOut).innerHTML = "<div><p>" + user + " likes " + fruit + "</p></div>";
